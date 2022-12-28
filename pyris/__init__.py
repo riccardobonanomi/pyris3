689be479dfba2a884a2833ccfbd18ffd96297f76
riccardobonanomi <riccardo.bonanomi@unitn.it>
2022-12-28 12:32:08 +0100

    adding mask cleaning to imported gee mask

--- pyris/__init__.py
+++ pyris/__init__.py
@@ -279,6 +279,38 @@ def import_gee_mask(geedir, geodir, maskdir ):
         # Loading geemask and georeferencing data
         mask, GeoTransf = LoadGeeMask( os.path.join( geedir, geemask ) )
 
+        print('applying BW masks...')
+
+        # GeoReferencing of White and Black masks
+        bw_masks_georef = GeoReference( GeoTransf )
+
+        # Apply Black Mask
+        black = np.ones( mask.shape, dtype=int )
+        black_masks = eval( config.get( 'Segmentation', 'black_masks' ) )
+        for s in black_masks:
+            xx, yy = bw_masks_georef.RefCurve( np.asarray(s[2:]), np.asarray(s[:2]), inverse=True )
+            sy, sx = slice( max(0, int(xx[0])), min(black.shape[1],int(xx[1])) ), slice( max(0,int(yy[0])), min(int(yy[1]),black.shape[0]) )
+            black[ sx, sy ] = 0
+
+        # Set Dimensions
+        pixel_width = config.getfloat('Data', 'channel_width') / GeoTransf['PixelSize'] # Channel width in Pixels
+        radius = 2 * pixel_width # Circle Radius for Local Thresholding
+
+        # Mask Landsat NoData
+        print('nodata dilation...')
+        nanmask = np.where( mask==0, 1, 0 )
+        nanmask = mm.binary_dilation( nanmask, mm.disk( 30 ) )
+        mask = np.where( nanmask==1, 0, mask*black )
+
+        # Image Cleaning
+        print('cleaning mask...')
+        mask = RemoveSmallObjects( mask, 100*pixel_width**2 ) # One Hundred Widths of Channel at Least is Required
+        radius = max( np.floor( 0.5 * ( pixel_width ) ) - 3, 0 )
+        mask = mm.binary_opening( mask, mm.disk( radius ) ) # Break Small Connectins
+        mask = CleanIslands( mask, 10*pixel_width**2 ) # Clean Holes Inside the Planform
+        mask = RemoveSmallObjects( mask, 100*pixel_width**2 ) # Remove New Small Objects
+        mask = mask.astype( int )
+
         print('saving  mask and GeoTransf data...')
         np.save( maskfile, mask )
         with open( geofile, 'wb' ) as gf: pickle.dump( GeoTransf, gf )
