081080ef8bec8649a04912655de2ae9e35c3ae59
riccardobonanomi <riccardo.bonanomi@unitn.it>
2023-03-20 17:37:48 +0100

    updated setup version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ long_description = '\n'.join((
 
 setup(
     name = 'pyris',
-    version = '3.1.0',
+    version = '3.1.1',
     author = 'Federico Monegaglia and Riccardo Bonanomi',
     author_email = 'f.monegaglia@gmail.com',
     maintainer = 'Riccardo Bonanomi',
